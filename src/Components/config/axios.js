d3a20ba627f0cee2597e1ce8119dbfb863d40aa2
kalkidan <kalkidanabreham214@gmail.com>
2025-01-06 01:43:36 +0300

    deploy

--- src/Components/config/axios.js
+++ src/Components/config/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-	baseURL: "http://localhost:4000/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "https://evangadi-forum-backend-1-rvth.onrender.com",
 });
 
 export default axiosBase;
